a340f8bb0ce4b858f5914595a0475a8b4cbf1d2c
subhashishjungshah <subhashishjungshah@gmail.com>
2024-01-01 12:42:54 +0545

    added cron to send email

--- src/cronjobs/cronjobs.module.ts
+++ src/cronjobs/cronjobs.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CronjobsService } from './cronjobs.service';
+import { MailModule } from 'src/mailer/mailer.module';
 
 @Module({
+  imports: [MailModule],
   providers: [CronjobsService],
 })
 export class CronjobsModule {}
--- src/cronjobs/cronjobs.service.ts
+++ src/cronjobs/cronjobs.service.ts
@@ -1,14 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { MailService } from 'src/mailer/mailer.service';
 
 @Injectable()
 export class CronjobsService {
   private readonly logger = new Logger(CronjobsService.name);
+  constructor(private prisma: PrismaService, private mailService: MailService) {}
 
-  @Cron(CronExpression.EVERY_30_SECONDS, {
-    name: 'Notify users',
+  @Cron(CronExpression.EVERY_10_SECONDS, {
+    name: 'Automated monthly-notification',
   })
-  handleCron() {
-    this.logger.debug('Called every 30 seconds');
+  async handleCron() {
+    this.logger.debug('Called every 10 seconds');
+    const users = await this.prisma.user.findMany({ where: { isActive: true } });
+    for (const user of users) {
+      // this.mailService.welcome({ email: user?.email, name: user?.name });
+      console.log(user.email);
+    }
   }
 }
